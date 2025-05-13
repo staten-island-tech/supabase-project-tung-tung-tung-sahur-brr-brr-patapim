9311522e0fe8db28f44ca5febf171128a80e0bc8
Eric <ericc404@nycstudents.net>
2025-05-13 14:00:16 -0400

    GAMEUI

--- supabase/src/App.vue
+++ supabase/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Terminal from './components/Terminal.vue'
+import GameUI from './components/GameUI.vue'
 </script>
 
 <template>
-  <Terminal />
+  <!-- <Terminal /> -->
+  <GameUI />
   <RouterView />
 </template>
---
+++ supabase/src/components/GameUI.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="background">
+    <div class="buttons-container">
+      <button>PLAY</button>
+      <button>ABOUT</button>
+      <button>SETTINGS</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  background-image: url('/pixel.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh;
+}
+.buttons-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.buttons-container button {
+  margin: 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  color: white;
+}
+</style>
