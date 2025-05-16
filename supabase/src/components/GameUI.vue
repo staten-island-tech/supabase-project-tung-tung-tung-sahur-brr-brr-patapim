0ea9af924324989abc214eb1a0d662175e329e14
Eric <ericc404@nycstudents.net>
2025-05-16 14:02:24 -0400

    Background and button styling

--- supabase/src/components/GameUI.vue
+++ supabase/src/components/GameUI.vue
@@ -1,35 +1,39 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="background">
-    <div class="buttons-container">
-      <button>PLAY</button>
-      <button>ABOUT</button>
-      <button>SETTINGS</button>
+  <div class="justify-center align-middle">
+    <div class="background">
+      <div class="buttons-container">
+        <button>
+          <img src="/buttons/Start.png" alt="" class="rounded-3xl" />
+        </button>
+        <button><img src="/buttons/Credits.png" alt="" class="rounded-3xl" /></button>
+        <button><img src="/buttons/Settings.png" alt="" class="rounded-3xl" /></button>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .background {
-  background-image: url('/pixel.png');
+  background-image: url('/Rome.webp');
   background-size: cover;
   background-repeat: no-repeat;
   width: 100%;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .buttons-container {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 100%;
+
+  transform: scale(0.3); /* Makes the container and its contents smaller */
 }
 
 .buttons-container button {
-  margin: 10px;
-  padding: 10px 20px;
-  font-size: 16px;
+  margin: 40px;
   cursor: pointer;
-  color: white;
 }
 </style>
